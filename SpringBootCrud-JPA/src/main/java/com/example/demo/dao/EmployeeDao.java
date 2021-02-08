63fe38d17d8cb4ecf91a8a50473b2db1c7524a9b
bAtmAn <bAtmAn@bAtmAn.mshome.net>
2021-02-08 14:38:38 +0530

    first commit uploading all files

---
+++ SpringBootCrud-JPA/src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -0,0 +1,16 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.entity.Employee;
+
+public interface EmployeeDao {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
